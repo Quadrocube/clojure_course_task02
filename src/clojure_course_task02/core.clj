114e5bb5a3f08dcafecc4ffc3a5949fbe995fe4e
Quadrocube <gliderok@gmail.com>
2013-03-26 17:13:36 +0300

    Solved second task

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,10 +1,21 @@
 (ns clojure-course-task02.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io])
+  (:use clojure.pprint))
+
+(defn pget-content [dir]
+  (cons dir
+    (mapcat deref 
+      (map #(future (pget-content %)) (.listFiles dir)))))
 
 
 (defn find-files [file-name path]
-  "TODO: Implement searching for a file using his name as a regexp."
-  nil)
+  "Parallel Search for a file using his name as a regexp."
+  (->> path 
+    io/file
+    pget-content
+    (pmap #(.getName %))
+    (filter #(re-matches (re-pattern file-name) %))))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
@@ -15,4 +26,5 @@
     (usage)
     (do
       (println "Searching for " file-name " in " path "...")
-      (dorun (map println (find-files file-name path))))))
+      (dorun (map println (find-files file-name path)))
+      (shutdown-agents))))
